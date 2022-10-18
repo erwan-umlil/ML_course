81d4476a4bcbd76f95ac3aa3a7389c63fcba0085
Erwan Umlil <erwan.umlil@polytechnique.edu>
2022-10-18 17:47:50 +0200

    lab 5

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -7,11 +7,24 @@ Least Square
 import numpy as np
 
 
+def compute_mse(y, tx, w):
+    """compute the loss by mse.
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        w: weights, numpy array of shape(D,), D is the number of features.
+    
+    Returns:
+        mse: scalar corresponding to the mse with factor (1 / 2 n) in front of the sum
+    """
+    e = y - tx.dot(w)
+    mse = e.dot(e) / (2 * len(e))
+    return mse
+
 def least_squares(y, tx):
     """calculate the least squares."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+    xtx = tx.T@tx
+    xty = tx.T@y
+    w = np.linalg.solve(xtx, xty)
+    mse = compute_mse(y, tx, w)
+    return w, mse
